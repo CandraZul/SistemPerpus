64191666d4bb2140a79ec2867914298b1a5ee355
Candra Zulkarnain <candrazulkarnain8@gmail.com>
2023-11-03 08:06:30 +0700

    Initial commit

---
+++ src/main/java/com/mycompany/sistemperpus/SistemPerpus.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemperpus;
+
+/**
+ *
+ * @author candr
+ */
+public class SistemPerpus {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
